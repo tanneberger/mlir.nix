3f336705bbe5394cfc68c4df5a5f5e956603b7c6
tanneberger <github@tanneberger.me>
2026-07-02 00:14:15 +0200

    use nixos 26.04

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Custom-Built MLIR";
 
   # Nixpkgs / NixOS version to use.
-  inputs.nixpkgs.url = "nixpkgs/nixos-25.05";
+  inputs.nixpkgs.url = "nixpkgs/nixos-26.05";
 
   outputs = { self, nixpkgs }:
     let
